e8dcb2116c532fcee4d2ffb958c3dd4501fd4994
Justin Huffaker <greyepoxy2@gmail.com>
2016-10-19 21:25:45 -0700

    removing typescript-build task. pulled it out into its own module.

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 exports.bundle = require('./gulptasks/bundle').bundle;
-exports.build = require('./gulptasks/typescript-build').build;
 exports.browserTests = require('./gulptasks/jasmine').browserTests;
